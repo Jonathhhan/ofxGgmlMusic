32b3881f183461cb27e06d79c716945fc8122903
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2026-05-16 02:21:53 +0200

    Use ImGui in analysis example
    Use ImGui in analysis example

--- ofxGgmlMusicAnalysisExample/src/ofApp.cpp
+++ ofxGgmlMusicAnalysisExample/src/ofApp.cpp
@@ -2,6 +2,7 @@
 
 void ofApp::setup() {
 	ofSetWindowTitle("ofxGgmlMusic smoke example");
+	gui.setup(nullptr, false);
 	request.audioPath = "audio/example.wav";
 	status = ofxGgmlMusicUtils::describe(request);
 	ofLogNotice("ofxGgmlMusicAnalysisExample") << status;
@@ -9,7 +10,15 @@ void ofApp::setup() {
 
 void ofApp::draw() {
 	ofBackground(18);
-	ofSetColor(240);
-	ofDrawBitmapString("ofxGgmlMusic", 32, 48);
-	ofDrawBitmapString(status, 32, 78);
+	gui.begin();
+	ImGui::SetNextWindowPos(ImVec2(24.0f, 24.0f), ImGuiCond_Once);
+	ImGui::SetNextWindowSize(ImVec2(560.0f, 220.0f), ImGuiCond_Once);
+	if (ImGui::Begin("ofxGgmlMusic Analysis Example")) {
+		ImGui::TextUnformatted("Audio Analysis Request");
+		ImGui::Separator();
+		ImGui::TextWrapped("%s", status.c_str());
+	}
+	ImGui::End();
+	gui.end();
+	gui.draw();
 }
